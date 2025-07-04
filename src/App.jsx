3764fd760322b3c763129b0dd2742afafd9aacf9
Mehakpreetkaur <mehakvirdi@gmail.com>
2025-07-04 05:20:07 +0000

    app file updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>FDP Demo CU By ByteXL</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
